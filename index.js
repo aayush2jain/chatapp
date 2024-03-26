28150c090211eaa919e762d7bce16777bb1f4e8b
aayush jain <aayushjain1290@gmail.com>
2024-03-26 17:36:34 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+
+const { Socket } = require('dgram');
+const express = require('express');
+const app = express(); 
+const path = require('path');
+const http =require('http').Server(app);
+var io =require('socket.io')(http);
+app.set('view engine','ejs');
+app.set('views','./')
+http.listen(3000,()=>{
+    console.log('server is running');
+});
+var users=[];
+var roomno=0;
+io.on('connection',function(socket){
+    socket.on('set-user-name',function(data){
+        // socket.on('disconnect',function(){
+        //     socket.broadcast.emit('user-disconnect',{username:data});
+        //     delete users[data];
+        // })
+        console.log(data+' user connected');
+    if(users.indexOf(data)>-1){
+        socket.emit('userexist',data +' username is already in use');
+    }
+    else{
+        users.push(data);
+        socket.emit('set-user',{username:data});
+    }
+    // socket.on('disconnect',function(data){
+    //      io.sockets.emit('user-disconnect',{username:data});
+    //      delete data;
+    // })
+     socket.on('message',function(data){
+      socket.broadcast.emit('new-msg',{username:data.user,message:data.message});
+       socket.emit('new-msg',{username:"you",message:data.message});
+    })
+     io.sockets.emit('user-connect',{username:data});//broadcast properly work nahi ker rha tha
+    });
+});
+app.get('/',function(req,res){
+    res.render('hello');
+})
